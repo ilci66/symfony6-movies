4a1f60d9025f3b0388871ff0fe31d3d123557503
unknown <akbiyik.ilker7@gmail.com>
2022-04-11 16:30:15 +0200

    can't change css values no idea why

--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -2,19 +2,40 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+use App\Entity\Movie;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class MoviesController extends AbstractController
 {
+    // using entity manageer like this is easier and faster
+    private $em;
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
     #[Route('/movies', name: 'app_movies')]
     public function index(): Response
     {
-        $movies = ["pokemon: 1", "pokemon: 2", "pokemon: 17"];
+        // $movie_repository = $this->em->getRepository(Movie::class);
 
-        return $this->render('movies/index.html.twig', 
-            array('movies' => $movies),
-        );
+        // $movies = $movie_repository->findAll();
+        // $movie_found = $movie_repository->find(9);
+        // $movies_descending = $movie_repository->findBy([], ['id' => 'DESC']);
+        // $movie_found_descending = $movie_repository->findOneBy(['id'=>10, 'title' => 'Samurai going to work'], ['id' => 'DESC']);
+
+        // $movies_count = $movie_repository->count([]);
+
+        // count the rows WHERE something = somrthing
+        // $movies_count = $repository->count(['id' => '10']);
+        // dd($movies_count); // ok this is cool, gives a lot of info
+
+
+        
+        return $this->render('movies/index.html.twig');
     }
 }
