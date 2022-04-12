4a236804033fc357b1c1a8d2e78ca11a464fb065
unknown <akbiyik.ilker7@gmail.com>
2022-04-12 15:01:45 +0200

    logging the input from rednered form

--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -4,9 +4,11 @@ namespace App\Controller;
 
 
 use App\Entity\Movie;
-use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\HttpFoundation\Response;
+use App\Form\MovieFormType;
 use App\Repository\MovieRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -29,6 +31,25 @@ class MoviesController extends AbstractController
 
         return $this->render('movies/index.html.twig', [
             'movies' => $movies
+        ]);
+    }
+
+    #[Route('/movies/create', name: 'create_movies')]
+    public function create(Request $request): Response
+    {
+        $movie = new Movie();
+        $form = $this->createForm(MovieFormType::class, $movie);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $newMovie = $form->getData();
+
+            dd($newMovie);
+            exit;
+        }
+
+
+        return $this->render('movies/create.html.twig', [
+            'form' => $form->createView()
         ]);
     }
 
