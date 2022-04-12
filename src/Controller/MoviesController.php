3e90c8bbea01808477f8ac89ec927b465a89ecc0
unknown <akbiyik.ilker7@gmail.com>
2022-04-12 13:23:57 +0200

    pages added & using cdn for tailwind

--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -6,6 +6,7 @@ namespace App\Controller;
 use App\Entity\Movie;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
+use App\Repository\MovieRepository;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -14,28 +15,30 @@ class MoviesController extends AbstractController
     // using entity manageer like this is easier and faster
     private $em;
 
-    public function __construct(EntityManagerInterface $em)
+    public function __construct(EntityManagerInterface $em, MovieRepository $movieRepository)
     {
         $this->em = $em;
+        $this->movieRepository = $movieRepository;
     }
     #[Route('/movies', name: 'app_movies')]
     public function index(): Response
     {
         // $movie_repository = $this->em->getRepository(Movie::class);
 
-        // $movies = $movie_repository->findAll();
-        // $movie_found = $movie_repository->find(9);
-        // $movies_descending = $movie_repository->findBy([], ['id' => 'DESC']);
-        // $movie_found_descending = $movie_repository->findOneBy(['id'=>10, 'title' => 'Samurai going to work'], ['id' => 'DESC']);
+        $movies = $this->movieRepository->findAll();
 
-        // $movies_count = $movie_repository->count([]);
+        return $this->render('movies/index.html.twig', [
+            'movies' => $movies
+        ]);
+    }
 
-        // count the rows WHERE something = somrthing
-        // $movies_count = $repository->count(['id' => '10']);
-        // dd($movies_count); // ok this is cool, gives a lot of info
-
-
+    #[Route('/movies/{id}', methods: ['GET'], name: 'show_movie')]
+    public function show($id): Response
+    {
+        $movie = $this->movieRepository->find($id);
         
-        return $this->render('movies/index.html.twig');
+        return $this->render('movies/show.html.twig', [
+            'movie' => $movie
+        ]);
     }
 }
